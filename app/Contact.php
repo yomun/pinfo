319909960f9c60ee9c6780ebcb00a2b81ce6dc34
Jason Mun <30671857+yomun@users.noreply.github.com>
2018-05-23 14:44:41 +0800

    Update Contact.php

--- app/Contact.php
+++ app/Contact.php
@@ -12,19 +12,20 @@ class Contact extends Model
 
 	protected $appends = ['name', 'email'];
 
-    public function user()
+	public function user()
 	{
 		return $this->belongsTo(User::class);
 	}
 
 	public function getNameAttribute()
-    {
-        $user = User::find($this->user_id);
-        return $user->name;
-    }
+	{
+		$user = User::find($this->user_id);
+		return $user->name;
+	}
 
-    public function getEmailAttribute() {
-    	$user = User::find($this->user_id);
-    	return $user->email;
-    }
+	public function getEmailAttribute() 
+	{
+		$user = User::find($this->user_id);
+		return $user->email;
+	}
 }
